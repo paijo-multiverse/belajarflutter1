0fa0a92b9b1b603bd2a15cba60cdc33420854eb3
paijo <squadpaijoganteng@gmail.com>
2024-09-02 17:30:45 +0800

    nambahin widget SingleChildScrollView

--- lib/login.dart
+++ lib/login.dart
@@ -21,98 +21,100 @@ class _LoginState extends State<Login> {
         title: const Text('Form Login'),
         backgroundColor: Colors.amber,
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(50),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            children: [
-              TextFormField(
-                controller: _emailController,
-                decoration: const InputDecoration(label: Text('Email')),
-                keyboardType: TextInputType.emailAddress,
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Email tidak boleh kosong';
-                  }
-                  return null;
-                },
-              ),
-              TextFormField(
-                controller: _passwordkController,
-                decoration: InputDecoration(
-                  label: const Text('Password'),
-                  suffixIcon: IconButton(
-                    onPressed: () {
-                      setState(() {
-                        _sembunyi = !_sembunyi;
-                      });
-                    },
-                    icon: Icon(
-                        _sembunyi ? Icons.visibility : Icons.visibility_off),
-                  ),
-                ),
-                obscureText: _sembunyi,
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Password tidak boleh kosong';
-                  }
-                  return null;
-                },
-              ),
-              const SizedBox(height: 20),
-              SizedBox(
-                width: double.infinity,
-                child: ElevatedButton(
-                  onPressed: () {
-                    if (_formKey.currentState!.validate()) {
-                      print('sebentar');
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(50),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              children: [
+                TextFormField(
+                  controller: _emailController,
+                  decoration: const InputDecoration(label: Text('Email')),
+                  keyboardType: TextInputType.emailAddress,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Email tidak boleh kosong';
                     }
+                    return null;
                   },
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.amber,
-                    foregroundColor: Colors.black,
-                    padding: const EdgeInsets.symmetric(vertical: 18),
-                  ),
-                  child: const Text('Masuk'),
                 ),
-              ),
-              const SizedBox(height: 5),
-              const Text('atau'),
-              const SizedBox(height: 5),
-              SizedBox(
-                width: double.infinity,
-                child: ElevatedButton(
-                  onPressed: () {},
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.amber,
-                    foregroundColor: Colors.black,
-                    padding: const EdgeInsets.symmetric(vertical: 18),
-                  ),
-                  child: const Text('Masuk dengan google'),
-                ),
-              ),
-              const SizedBox(height: 10),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  const Text('Belum punya akun ?'),
-                  const SizedBox(width: 8),
-                  GestureDetector(
-                    onTap: () {
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => const Daftar()));
-                    },
-                    child: const Text(
-                      'Daftar',
-                      style: TextStyle(color: Colors.blue),
+                TextFormField(
+                  controller: _passwordkController,
+                  decoration: InputDecoration(
+                    label: const Text('Password'),
+                    suffixIcon: IconButton(
+                      onPressed: () {
+                        setState(() {
+                          _sembunyi = !_sembunyi;
+                        });
+                      },
+                      icon: Icon(
+                          _sembunyi ? Icons.visibility : Icons.visibility_off),
                     ),
                   ),
-                ],
-              ),
-            ],
+                  obscureText: _sembunyi,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Password tidak boleh kosong';
+                    }
+                    return null;
+                  },
+                ),
+                const SizedBox(height: 20),
+                SizedBox(
+                  width: double.infinity,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      if (_formKey.currentState!.validate()) {
+                        print('sebentar');
+                      }
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.amber,
+                      foregroundColor: Colors.black,
+                      padding: const EdgeInsets.symmetric(vertical: 18),
+                    ),
+                    child: const Text('Masuk'),
+                  ),
+                ),
+                const SizedBox(height: 5),
+                const Text('atau'),
+                const SizedBox(height: 5),
+                SizedBox(
+                  width: double.infinity,
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.amber,
+                      foregroundColor: Colors.black,
+                      padding: const EdgeInsets.symmetric(vertical: 18),
+                    ),
+                    child: const Text('Masuk dengan google'),
+                  ),
+                ),
+                const SizedBox(height: 10),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text('Belum punya akun ?'),
+                    const SizedBox(width: 8),
+                    GestureDetector(
+                      onTap: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => const Daftar()));
+                      },
+                      child: const Text(
+                        'Daftar',
+                        style: TextStyle(color: Colors.blue),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
